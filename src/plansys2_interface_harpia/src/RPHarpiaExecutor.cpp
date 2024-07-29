5e0b6aafe66b592bc4e0721aadc12c888ed7b919
Karine <karine3343@gmail.com>
2024-07-29 20:30:06 -0300

    Interface

--- src/plansys2_interface_harpia/src/RPHarpiaExecutor.cpp
+++ src/plansys2_interface_harpia/src/RPHarpiaExecutor.cpp
@@ -121,13 +121,14 @@ void Mission::chatterCallback_wpqtd(const mavros_msgs::msg::WaypointList::Shared
 void Mission::chatterCallback_IDGoal(const interfaces::action::MissionPlanner::Goal::SharedPtr msg)
 {
     if (!msg->mission.goals.empty()) {
-        IDGoal = std::stoi(msg->mission.goals[0].id);
+        IDGoal = std::stoi(msg->mission.goals[0].goalID.id); 
         std::string action = msg->mission.goals[0].action;
         std::string region = msg->mission.goals[0].region;
 
         // Faça algo com `action` e `region` se necessário
     }
 }
+
 
 
 void Mission::chatterCallback_current(const mavros_msgs::msg::WaypointReached::SharedPtr msg)
