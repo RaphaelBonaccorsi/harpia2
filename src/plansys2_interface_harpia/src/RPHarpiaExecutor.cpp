a63a7ef10b964a54c35e4dd32958d4dcf5ee0f6f
Karine <karine3343@gmail.com>
2024-07-29 18:55:44 -0300

    Interface

--- src/plansys2_interface_harpia/src/RPHarpiaExecutor.cpp
+++ src/plansys2_interface_harpia/src/RPHarpiaExecutor.cpp
@@ -1,4 +1,4 @@
-#include "plansys2_interface_harpia/RPHarpiaExecutor.hpp"
+#include "rosplan_interface_harpia/RPHarpiaExecutor.hpp"
 
 #include "std_msgs/msg/string.hpp"
 #include "std_srvs/srv/empty.hpp"
