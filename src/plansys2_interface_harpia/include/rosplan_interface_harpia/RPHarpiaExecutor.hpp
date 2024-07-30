571c6d7e07fe8e258c11ec1d4f6cc7a72398aab0
Karine <karine3343@gmail.com>
2024-07-29 22:03:38 -0300

    Interface

--- src/plansys2_interface_harpia/include/rosplan_interface_harpia/RPHarpiaExecutor.hpp
+++ src/plansys2_interface_harpia/include/rosplan_interface_harpia/RPHarpiaExecutor.hpp
@@ -11,11 +11,7 @@ namespace plansys2
     class RPHarpiaExecutor : public plansys2::ActionExecutorClient
     {
     public:
-        RPHarpiaExecutor()
-            : plansys2::ActionExecutorClient("rpharpia_executor", std::chrono::seconds(1))
-        {
-            this->declare_parameter<double>("action_duration", 2.0);
-        }
+        RPHarpiaExecutor();
 
         void do_work() override
         {
