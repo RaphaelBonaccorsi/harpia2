f8ddc5be3fd9f2143210b1f333421b889afe34e4
Karine <karine3343@gmail.com>
2024-07-29 21:31:33 -0300

    Interface

--- src/plansys2_interface_harpia/include/rosplan_interface_harpia/RPHarpiaExecutor.hpp
+++ src/plansys2_interface_harpia/include/rosplan_interface_harpia/RPHarpiaExecutor.hpp
@@ -1,31 +1,39 @@
-#ifndef HARPIA_EXECUTOR_HPP_
-#define HARPIA_EXECUTOR_HPP_
+#ifndef HARPIA_EXECUTOR
+#define HARPIA_EXECUTOR
 
 #include <rclcpp/rclcpp.hpp>
 #include <plansys2_executor/ActionExecutorClient.hpp>
 #include <plansys2_msgs/msg/action_execution_info.hpp>
-#include <interfaces/srv/mission_fault_mitigation.hpp>
-#include <mavros_msgs/srv/waypoint_push.hpp>
-#include <mavros_msgs/srv/waypoint_clear.hpp>
 
 namespace plansys2
 {
 
-class RPHarpiaExecutor : public plansys2::ActionExecutorClient
-{
-public:
-    RPHarpiaExecutor();
+    class RPHarpiaExecutor : public plansys2::ActionExecutorClient
+    {
+    public:
+        RPHarpiaExecutor()
+            : plansys2::ActionExecutorClient("rpharpia_executor", std::chrono::seconds(1))
+        {
+            this->declare_parameter<double>("action_duration", 2.0);
+        }
 
-    void do_work() override;
+        void do_work() override
+        {
+            // Crie um feedback
+            auto feedback = std::make_shared<plansys2_msgs::msg::ActionExecutionInfo>();
+            feedback->status = plansys2_msgs::msg::ActionExecutionInfo::EXECUTING;
+            feedback->completion = 0.5;
 
-private:
-    rclcpp::Client<interfaces::srv::MissionFaultMitigation>::SharedPtr mission_fault_client_;
-    rclcpp::Client<mavros_msgs::srv::WaypointPush>::SharedPtr waypoint_push_client_;
-    rclcpp::Client<mavros_msgs::srv::WaypointClear>::SharedPtr waypoint_clear_client_;
+            // Envie o feedback usando os valores apropriados
+            send_feedback(feedback->completion, "Executing");
 
-    // Adicione outros membros privados conforme necessário
-};
+            // Adicione sua lógica de execução aqui
+
+            // Finalize a ação com sucesso
+            finish(true, 1.0, "Action completed successfully");
+        }
+    };
 
 } // namespace plansys2
 
-#endif // HARPIA_EXECUTOR_HPP_
+#endif // HARPIA_EXECUTOR
