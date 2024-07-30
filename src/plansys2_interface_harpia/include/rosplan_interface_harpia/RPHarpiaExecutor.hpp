789116b372e7b308093c5bceb50e5e244a613c3a
Karine <karine3343@gmail.com>
2024-07-29 22:09:38 -0300

    Interface

--- src/plansys2_interface_harpia/include/rosplan_interface_harpia/RPHarpiaExecutor.hpp
+++ src/plansys2_interface_harpia/include/rosplan_interface_harpia/RPHarpiaExecutor.hpp
@@ -11,7 +11,21 @@ namespace plansys2
     class RPHarpiaExecutor : public plansys2::ActionExecutorClient
     {
     public:
-        RPHarpiaExecutor();
+        RPHarpiaExecutor::RPHarpiaExecutor()
+            : plansys2::ActionExecutorClient("rpharpia_executor", std::chrono::seconds(1))
+        {
+            mission_fault_client_ = this->create_client<interfaces::srv::MissionFaultMitigation>("harpia/mission_fault_mitigation");
+            waypoint_push_client_ = this->create_client<mavros_msgs::srv::WaypointPush>("mavros/mission/push");
+            waypoint_clear_client_ = this->create_client<mavros_msgs::srv::WaypointClear>("mavros/mission/clear");
+
+            // Declaração de parâmetros
+            this->declare_parameter<double>("action_duration", 2.0);
+
+            // Inicialização do feedback
+            auto feedback = std::make_shared<plansys2_msgs::msg::ActionExecutionFeedback>();
+            feedback->progress = 0.0;
+            send_feedback(feedback);
+        }
 
         void do_work() override
         {
--- src/plansys2_interface_harpia/src/RPHarpiaExecutor.cpp
+++ src/plansys2_interface_harpia/src/RPHarpiaExecutor.cpp
@@ -653,22 +653,6 @@ void mySigintHandler(int)
 namespace plansys2
 {
 
-    RPHarpiaExecutor::RPHarpiaExecutor()
-       : plansys2::ActionExecutorClient("rpharpia_executor", std::chrono::seconds(1))
-    {
-        mission_fault_client_ = this->create_client<interfaces::srv::MissionFaultMitigation>("harpia/mission_fault_mitigation");
-        waypoint_push_client_ = this->create_client<mavros_msgs::srv::WaypointPush>("mavros/mission/push");
-        waypoint_clear_client_ = this->create_client<mavros_msgs::srv::WaypointClear>("mavros/mission/clear");
-
-        // Declaração de parâmetros
-        this->declare_parameter<double>("action_duration", 2.0);
-
-        // Inicialização do feedback
-        auto feedback = std::make_shared<plansys2_msgs::msg::ActionExecutionFeedback>();
-        feedback->progress = 0.0;
-        send_feedback(feedback);
-    }
-
     void RPHarpiaExecutor::do_work(){
         auto msg = get_goal();
 
