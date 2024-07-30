2df88b0c8a9ad927abcc9ae81670bcf350ef3256
Karine <karine3343@gmail.com>
2024-07-30 14:17:19 -0300

    Interface

--- src/plansys2_interface_harpia/include/rosplan_interface_harpia/RPHarpiaExecutor.hpp
+++ src/plansys2_interface_harpia/include/rosplan_interface_harpia/RPHarpiaExecutor.hpp
@@ -1,6 +1,9 @@
 #ifndef HARPIA_EXECUTOR
 #define HARPIA_EXECUTOR
 
+#ifndef RP_HARPIA_EXECUTOR
+#define RP_HARPIA_EXECUTOR
+
 #include <rclcpp/rclcpp.hpp>
 #include <plansys2_executor/ActionExecutorClient.hpp>
 #include <plansys2_msgs/msg/action_execution_info.hpp>
@@ -10,16 +13,17 @@ namespace plansys2
 
     class RPHarpiaExecutor : public plansys2::ActionExecutorClient
     {
-    public:
-        RPHarpiaExecutor()
-            : plansys2::ActionExecutorClient("rpharpia_executor", std::chrono::seconds(1))
-        {
-            this->declare_parameter<double>("action_duration", 2.0);
-        }
+    private:
+        // Aqui você pode adicionar membros privados, se necessário
 
+    public:
+        /* construtor */
+        RPHarpiaExecutor();
+
+        /* método para executar o trabalho */
         void do_work() override;
     };
 
 } // namespace plansys2
 
-#endif // HARPIA_EXECUTOR
+#endif // RP_HARPIA_EXECUTOR
